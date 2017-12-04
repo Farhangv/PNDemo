c7d78ce8c1abf21853b1891e7bbc45ff0720c9d8
Farhang Vafa <farhangv@ymail.com>
2017-12-04 15:05:03 +0330

    960912

---
+++ Session01-960828/MVCDemo/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    [Table("User")]
+    public class User
+    {
+        public int Id { get; set; }
+        [Index(IsUnique = true), MaxLength(50), MinLength(5), Display(Name = "نام کاربری")]
+        public string Username { get; set; }
+        [Display(Name = "کلمه عبور"), MinLength(8)]
+        public string Password { get; set; }
+        [MaxLength(50), Display(Name = "نام")]
+        public string Name { get; set; }
+        [MaxLength(50), Display(Name = "نام خانوادگی")]
+        public string Family { get; set; }
+    }
+}
